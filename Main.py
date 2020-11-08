62fb9a25ea9e52e09f092fedd5f9cd361282ac0a
Moh. Milki I. M <73022578+mohmilki@users.noreply.github.com>
2020-11-08 20:52:10 +0700

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,8 @@
+#perintah untuk menampilkan data diri
+print("Nama         : Moh. Milki I. M")
+print("NIM          : 10220013")
+print("Mata Kuliah  : Algoritma dan Struktur data")
+
+#Menampilkan perintah hello world
+
+print("Hello World!")
